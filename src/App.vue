9b195c022ab6501f67bfefb8ca0d9b65ef9a3e8f
guoqinliang <guoqinliang@golaxy.cn>
2023-06-26 17:37:58 +0800

    删除无用代码，只有日历

--- src/App.vue
+++ src/App.vue
@@ -1,71 +1,41 @@
 <template>
   <div id="app">
-    <el-container>
-      <el-header>Header</el-header>
-      <el-container>
-        <el-aside width="auto">
-          <el-radio-group v-model="isCollapse" style="margin-bottom: 20px;width:70px;">
-            <el-radio-button :label="false">展开</el-radio-button>
-            <el-radio-button :label="true">收起</el-radio-button>
-          </el-radio-group>
-          <el-menu
-            default-active="1-4-1"
-            class="el-menu-vertical-demo"
-            @open="handleOpen"
-            @close="handleClose"
-            @select="handleSelect"
-            :collapse="isCollapse"
-          >
-            <el-submenu index="1">
-              <template slot="title">
-                <i class="el-icon-location"></i>
-                <span slot="title">导航一</span>
-              </template>
-              <el-menu-item-group>
-                <el-menu-item index="1-1"><router-link to="/menu1">Go to Menu1</router-link></el-menu-item>
-                <el-menu-item index="1-2"><router-link to="/menu2">Go to Menu2</router-link></el-menu-item>
-                <el-menu-item index="1-3">选项3</el-menu-item>
-              </el-menu-item-group>
-              <el-submenu index="1-4">
-                <span slot="title">选项4</span>
-                <el-menu-item index="1-4-1">选项1</el-menu-item>
-              </el-submenu>
-            </el-submenu>
-            <el-menu-item index="2">
-              <i class="el-icon-menu"></i>
-              <span slot="title">导航二</span>
-            </el-menu-item>
-          </el-menu>
-        </el-aside>
-        <el-main>
-          <router-view></router-view>
-        </el-main>
-      </el-container>
-    </el-container>
+    <el-calendar>
+      <!-- 这里使用的是 2.5 slot 语法，对于新项目请使用 2.6 slot 语法-->
+      <template slot="dateCell" slot-scope="{ date, data }">
+        <div style="text-align:center">
+          {{
+            data.day
+              .split('-')
+              .slice(2)
+              .join('-')
+          }}
+          <div>{{ getStatu(date) }}</div>
+        </div>
+      </template>
+    </el-calendar>
   </div>
 </template>
 
 <script>
 export default {
-  name: "app",
+  name: 'app',
   components: {},
   data() {
     return {
-      isCollapse: true
-    };
+      data1: '2023-06-25'
+    }
   },
   methods: {
-    handleOpen(key, keyPath) {
-      console.log(key, keyPath);
+    getStatu(date) {
+      let arr = ['治', '白', '夜', '休', '休']
+      return arr[this.DateDiff(date) % 5]
     },
-    handleClose(key, keyPath) {
-      console.log(key, keyPath);
-    },
-    handleSelect(key, keyPath){
-      console.log(key, keyPath);
+    DateDiff(date2) {
+      return (date2.getTime() - new Date(this.date1).getTime()) / (1000 * 3600 * 24)
     }
   }
-};
+}
 </script>
 
 <style>
@@ -73,21 +43,7 @@ export default {
   margin: 0;
   padding: 0;
 }
-html,body,#app,.el-container{
-  height: 100%;
-}
-header {
-  background: #3367d6;
-}
-.el-aside {
-  border-right: 1px solid #ccc;
-  height: 100%;
-}
-#app .el-menu {
-  border-right: none;
-}
-.el-menu-vertical-demo:not(.el-menu--collapse) {
-  width: 200px;
-  min-height: 400px;
+.current.is-today {
+  background: rgb(242, 248, 254);
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,22 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
-import { Menu,Submenu,MenuItem,MenuItemGroup,Header, Aside ,Main,Container,Message,RadioGroup,RadioButton} from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
-// import VueRouter from 'vue-router'
-import router from './router';
-Vue.use(Menu)
-Vue.use(Submenu)
-Vue.use(MenuItem)
-Vue.use(MenuItemGroup)
-Vue.use(Header)
-Vue.use(Aside)
-Vue.use(Main)
-Vue.use(Container)
-Vue.use(RadioGroup)
-Vue.use(RadioButton)
+import { Calendar } from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(Calendar)
 Vue.config.productionTip = false
-Vue.prototype.$message = Message;
 new Vue({
-  render: h => h(App),
-  router,
+  render: h => h(App)
 }).$mount('#app')
